1523a5e459fa817d397c526c30322310e9a35dfa
Ronny230797 <ronny230797@gmail.com>
2018-12-14 11:02:18 -0600

    work csv

--- React/ClientApp/src/Reportes/MyCsvLink.js
+++ React/ClientApp/src/Reportes/MyCsvLink.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { CSVLink, CSVDownload } from "react-csv";
 import '../components/ButtonColor.css';
+import axios from 'axios';
 
 
 
@@ -8,13 +9,22 @@ class MyCsvLink extends React.Component {
     csvLink = React.createRef()
     state = { data: [] }
   
-    fetchData = () => {
+    /*fetchData = () => {
       fetch('/mydata').then(data => {
         this.setState({ data }, () => {
           // click the CSVLink component to trigger the CSV download
           this.csvLink.current.link.click()
         })
       })
+    }*/
+
+    componentDidMount() {
+        axios.get(`http://localhost:56461/api/Reporte/ObtenerCsv`)
+            .then(res => {
+                const data = res.data;
+                this.setState({ data });
+                this.csvLink.current.link.click()
+            })
     }
   
     render() {
@@ -37,7 +47,12 @@ class MyCsvLink extends React.Component {
       return (
         <div>
           <button className="btn btnBlue" onClick={this.fetchData}>Descargar CSV</button>
-  
+
+              {/*this.state.incidents.map(incident => <ColumnSeries name={incident.mes} data={[{ name: incident.mes, y: incident.cantidadIncidentes },]}
+              />)*/}
+              {/*this.state.incidents.map(incident => <CSVLink columns={incident.mes} data={[{ name: incident.mes, y: incident.cantidadIncidentes },]}
+                  />)*/} 
+
           <CSVLink
             data={datas}
             separator=";"
